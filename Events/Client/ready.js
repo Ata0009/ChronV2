326b53c8b9438fd3c9112b4ebcef5143accfaa5c
Ata0009 <88573429+Ata0009@users.noreply.github.com>
2022-08-22 19:16:43 +1000

    Update ready.js

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,24 +1,9 @@
+const { loadCommands } = require("../../Handlers/commandHandler");
+
 module.exports = {
   name: "ready",
   once: true,
-  execute() {
+  execute(client) {
     console.log(`Client Successfully Logged In | ChronV1 Loaded | OK 200`);
-        const activities = [
-      "with the &help command.",
-      "with the developers console.",
-      "with some code.",
-      "with JavaScript."
-    ];
-
-    bot.on("ready", () => {
-      // run every 10 seconds
-      setInterval(() => {
-        // generate random number between 1 and list length.
-        const randomIndex = Math.floor(Math.random() * (activities.length - 1) + 1);
-        const newActivity = activities[randomIndex];
-
-        bot.user.setActivity(newActivity);
-      }, 10000);
-    });
   },
 };
